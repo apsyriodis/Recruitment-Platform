9f389559f1a87c6269e3badd00891819b3d10de0
Apostolos Syriodis <apsyriodis@gmail.com>
2023-11-25 17:46:55 +0200

    Changes on UserFactory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -21,10 +21,10 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+            'name' => 'Apostolos Syriodis',
+            'email' => 'apsyriodis@gmail.com',
             'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
+            'password' => '1234!@#$',
             'remember_token' => Str::random(10),
         ];
     }
